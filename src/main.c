a8fa44010b48ddd2cec87cf9d05a072bc7937eb7
CsatiZoltan <csati_zoltan@freemail.hu>
2016-08-21 15:05:11 +0200

    Tested gregorian.c on today's date.

--- src/main.c
+++ src/main.c
@@ -1,10 +1,31 @@
-/* Run the test dates
-   Serves as a tutorial for the use of the provided functions */
+/* Run for the date of today */
+
+#include <stdio.h>
+#include <time.h>
 
 int gregorian(int year, int month, int day);
 
 int main(void)
 {
+    /* Test the algorithm with the current date */
+    int dayToday, monthToday, yearToday;
+    struct tm *currentTime;
+    time_t timer;
+    time(&timer);
+    currentTime = localtime(&timer);
+    dayToday = currentTime->tm_mday;
+    monthToday = currentTime->tm_mon + 1;
+    yearToday = currentTime->tm_year + 1900;
+    int realDay = currentTime->tm_wday;
+
+    /* Compare it with the calculated date */
+    if (realDay == 0)
+        realDay = 7;
+    int computedDay = gregorian(yearToday ,monthToday, dayToday);
+    if (computedDay == realDay)
+        printf("The calculated and the extracted day agrees.\n");
+    else
+        printf("The algorithm fails for the current day.\n");
 
     return 0;
 }
