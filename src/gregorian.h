833f0d21fdc5b8b7c2f2038f139c7ea4ac282d48
CsatiZoltan <csati_zoltan@freemail.hu>
2016-09-15 17:54:13 +0200

    Created a header file as gregorian.c will be needed for the GUI version too.

---
+++ src/gregorian.h
@@ -0,0 +1,16 @@
+#ifndef GREGORIAN_H
+#define GREGORIAN_H
+
+/* Extern "C" */
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int gregorian(int year, int month, int day);
+
+/* Extern "C" */
+#ifdef __cplusplus
+}
+#endif
+
+#endif
