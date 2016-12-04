ffd7560ec8e681e18a80edf7c7854d6c8848950d
CsatiZoltan <csati_zoltan@freemail.hu>
2016-12-04 11:04:19 +0100

    Updated Hungarian translation

---
+++ GUI/translations/hun.ts
@@ -0,0 +1,164 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="hu_HU" sourcelanguage="en">
+<context>
+    <name>QCoreApplication</name>
+    <message>
+        <location filename="zellergui.cpp" line="110"/>
+        <source>Usage: select the date (year, month and day) you wish
+ to know on which day of the week it was (or will be)
+ according to the Gregorian or the Julian calendar that
+ can be selected on the right. The typed data can be 
+ accepted by pressing Enter or clicking into another field.
+
+
+Created by
+Zoltan Csati</source>
+        <translation>Használat: válassza ki azt a dátumot (év, hónap, nap) amelyikről tudni szeretné,
+hogy a hét melyik napján volt (vagy lesz) a Gergely vagy a Julián naptár alapján,
+ami a jobb oldalon választható ki. A begépelt adat az Enter megnyomásával vagy
+egy másik mezőbe klikkeléssel fogadtatható el.
+
+
+Készítette:
+Csáti Zoltán</translation>
+    </message>
+</context>
+<context>
+    <name>Weekdays</name>
+    <message>
+        <location filename="zellergui.cpp" line="18"/>
+        <source>Monday</source>
+        <translation>hétfő</translation>
+    </message>
+    <message>
+        <location filename="zellergui.cpp" line="19"/>
+        <source>Tuesday</source>
+        <translation>kedd</translation>
+    </message>
+    <message>
+        <location filename="zellergui.cpp" line="20"/>
+        <source>Wednesday</source>
+        <translation>szerda</translation>
+    </message>
+    <message>
+        <location filename="zellergui.cpp" line="21"/>
+        <source>Thursday</source>
+        <translation>csütörtök</translation>
+    </message>
+    <message>
+        <location filename="zellergui.cpp" line="22"/>
+        <source>Friday</source>
+        <translation>péntek</translation>
+    </message>
+    <message>
+        <location filename="zellergui.cpp" line="23"/>
+        <source>Saturday</source>
+        <translation>szombat</translation>
+    </message>
+    <message>
+        <location filename="zellergui.cpp" line="24"/>
+        <source>Sunday</source>
+        <translation>vasárnap</translation>
+    </message>
+</context>
+<context>
+    <name>ZellerGUI</name>
+    <message>
+        <location filename="zellergui.ui" line="41"/>
+        <source>Day of week</source>
+        <translation>Öröknaptár</translation>
+    </message>
+    <message>
+        <location filename="zellergui.ui" line="64"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Year of interest&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>Keresett év</translation>
+    </message>
+    <message>
+        <location filename="zellergui.ui" line="67"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Year&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>Év</translation>
+    </message>
+    <message>
+        <location filename="zellergui.ui" line="112"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Month of interest&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>Keresett hónap</translation>
+    </message>
+    <message>
+        <location filename="zellergui.ui" line="115"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Month&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>Hónap</translation>
+    </message>
+    <message>
+        <location filename="zellergui.ui" line="129"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Day of interest&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>Keresett nap</translation>
+    </message>
+    <message>
+        <location filename="zellergui.ui" line="132"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Day&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>Nap</translation>
+    </message>
+    <message>
+        <location filename="zellergui.ui" line="74"/>
+        <source>1848</source>
+        <translation>1848</translation>
+    </message>
+    <message>
+        <location filename="zellergui.ui" line="122"/>
+        <source>3</source>
+        <translation>3</translation>
+    </message>
+    <message>
+        <location filename="zellergui.ui" line="139"/>
+        <source>15</source>
+        <translation>15</translation>
+    </message>
+    <message>
+        <location filename="zellergui.ui" line="83"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Gregorian or Julian calendar&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>Gergely vagy Julián naptár</translation>
+    </message>
+    <message>
+        <location filename="zellergui.ui" line="87"/>
+        <source>Gregorian</source>
+        <translation>Gergely</translation>
+    </message>
+    <message>
+        <location filename="zellergui.ui" line="92"/>
+        <source>Julian</source>
+        <translation>Julián</translation>
+    </message>
+    <message>
+        <location filename="zellergui.ui" line="100"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Click for help&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>Kattintson segítségért</translation>
+    </message>
+    <message>
+        <location filename="zellergui.ui" line="103"/>
+        <location filename="zellergui.cpp" line="48"/>
+        <source>Help</source>
+        <translation>Segítség</translation>
+    </message>
+    <message>
+        <location filename="zellergui.ui" line="148"/>
+        <source>Day of the week</source>
+        <translation>A hét napja</translation>
+    </message>
+    <message>
+        <location filename="zellergui.ui" line="155"/>
+        <source>Wednesday</source>
+        <translation>szerda</translation>
+    </message>
+    <message>
+        <location filename="zellergui.cpp" line="127"/>
+        <source>Info</source>
+        <translation>Segítég</translation>
+    </message>
+    <message>
+        <location filename="zellergui.cpp" line="127"/>
+        <source>Calendar not yet implemented.</source>
+        <translation>Naptár még nincs implementálva.</translation>
+    </message>
+</context>
+</TS>
